f41bf6aa021885f66bb9a156ec3d43918f4d72e4
gustavo7lagoas <gustavo7lagoas@gmail.com>
2019-08-05 10:24:35 +0100

    commit with tests

---
+++ movies/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """ GET / must return 200 """
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template(self):
+        """ Must use movies_stuff.html template """
+        self.assertTemplateUsed(self.response, 'movies/movies_stuff.html')
+
+    def test_html(self):
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<img')
---
+++ movies/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib import messages
+from airtable import Airtable
+import os
+
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+    'Movies',
+    api_key=os.environ.get('AIRTABLE_API_KEY'))
+
+def home_page(request):
+    user_query = str(request.GET.get('query', ''))
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
+    context_dictionary = {'search_result': search_result}
+    return render(request, 'movies/movies_stuff.html', context_dictionary)
